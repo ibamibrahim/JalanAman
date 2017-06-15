c8b6d68b3b2896fb2b53098891b34b194852cda6
Ibam <ibamibrahim0@gmail.com>
2017-06-15 15:04:18 +0700

    add tab

--- app/src/main/java/id/aicode/jalanaman/MainActivity.java
+++ app/src/main/java/id/aicode/jalanaman/MainActivity.java
@@ -1,13 +1,133 @@
 package id.aicode.jalanaman;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    @BindView(R.id.tabs)
+    TabLayout tabLayout;
+
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        tabLayout.setupWithViewPager(mViewPager);
+        setTabIcon();
+    }
+
+    protected void setTabIcon(){
+        tabLayout.getTabAt(0).setIcon(R.drawable.myplace_selected);
+        tabLayout.getTabAt(1).setIcon(R.drawable.clock_selected);
+        tabLayout.getTabAt(2).setIcon(R.drawable.emergency_selected);
+
+    }
+
+/*
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+*/
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+       /* if (id == R.id.action_settings) {
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position){
+                case 0:
+                    MyPlaceTabFragment tab1 = new MyPlaceTabFragment();
+                    return tab1;
+                case 1:
+                    RecentTabFragment tab2 = new RecentTabFragment();
+                    return tab2;
+                case 2:
+                    EmergencyCallFragment tab3 = new EmergencyCallFragment();
+                    return tab3;
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            // Show 4 total pages.
+            return 3;
+        }
+
+        /*@Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getString(R.string.tab1_name);
+                case 1:
+                    return getString(R.string.tab2_name);
+                case 2:
+                    return getString(R.string.tab3_name);
+                case 3:
+                    return getString(R.string.tab4_name);
+            }
+            return null;
+        }*/
     }
 }
+
