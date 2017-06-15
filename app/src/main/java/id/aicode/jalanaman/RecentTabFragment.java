32d5dd3c746ad4ce0e38de85cedc5c2c49f69667
Ibam <ibamibrahim0@gmail.com>
2017-06-15 20:04:40 +0700

    done tab recent

--- app/src/main/java/id/aicode/jalanaman/RecentTabFragment.java
+++ app/src/main/java/id/aicode/jalanaman/RecentTabFragment.java
@@ -14,6 +14,6 @@ public class RecentTabFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_my_place, container, false);
+        return inflater.inflate(R.layout.tab_recent, container, false);
     }
 }
