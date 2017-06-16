8066a713b47589bee0c5a7a2b92e39633e406493
Ibam <ibamibrahim0@gmail.com>
2017-06-16 11:28:44 +0700

    tweak2 dikit

--- app/src/main/java/id/aicode/jalanaman/MyPlaceTabFragment.java
+++ app/src/main/java/id/aicode/jalanaman/MyPlaceTabFragment.java
@@ -5,15 +5,40 @@ package id.aicode.jalanaman;
  */
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MyPlaceTabFragment extends Fragment {
 
+    @BindView(R.id.add_place)
+    FloatingActionButton floatingActionButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_my_place, container, false);
+        View view = inflater.inflate(R.layout.tab_my_place, container, false);
+        ButterKnife.bind(this, view);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
+                final View dialogView = getActivity().getLayoutInflater().inflate(R.layout
+                        .dialog_add_route,
+                        null);
+
+                dialogBuilder.setView(dialogView);
+                final AlertDialog dialog = dialogBuilder.create();
+                dialog.show();
+            }
+        });
+
+        return view;
     }
 }
