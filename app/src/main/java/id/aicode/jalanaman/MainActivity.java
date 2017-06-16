3e322c313a91d87e286c8b24dc293a2f0a7d783a
Ibam <ibamibrahim0@gmail.com>
2017-06-16 15:23:18 +0700

    add map

--- app/src/main/java/id/aicode/jalanaman/MainActivity.java
+++ app/src/main/java/id/aicode/jalanaman/MainActivity.java
@@ -46,15 +46,47 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
         tabLayout.setupWithViewPager(mViewPager);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(position == 0){
+                    setTabIcon();
+                } else if (position == 1){
+                    setTabIcon2();
+                } else {
+                    setTabIcon3();
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
         setTabIcon();
     }
 
     protected void setTabIcon(){
         tabLayout.getTabAt(0).setIcon(R.drawable.myplace_selected);
+        tabLayout.getTabAt(1).setIcon(R.drawable.clock);
+        tabLayout.getTabAt(2).setIcon(R.drawable.emergency);
+    } protected void setTabIcon2(){
+        tabLayout.getTabAt(0).setIcon(R.drawable.my_place);
         tabLayout.getTabAt(1).setIcon(R.drawable.clock_selected);
+        tabLayout.getTabAt(2).setIcon(R.drawable.emergency);
+    } protected void setTabIcon3(){
+        tabLayout.getTabAt(0).setIcon(R.drawable.my_place);
+        tabLayout.getTabAt(1).setIcon(R.drawable.clock);
         tabLayout.getTabAt(2).setIcon(R.drawable.emergency_selected);
-
     }
+
+
 
 /*
 
