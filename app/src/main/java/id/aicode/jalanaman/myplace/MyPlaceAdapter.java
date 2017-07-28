855c09a5e59de82872d5497cd682303d7afce564
Ibam <ibamibrahim0@gmail.com>
2017-07-28 17:15:54 +0700

    done myplace tab stub and adapter

---
+++ app/src/main/java/id/aicode/jalanaman/myplace/MyPlaceAdapter.java
@@ -0,0 +1,153 @@
+package id.aicode.jalanaman.myplace;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import id.aicode.jalanaman.R;
+import id.aicode.jalanaman.recent.RecentEventAdapter;
+import id.aicode.jalanaman.services.models.event.EventResponse;
+import id.aicode.jalanaman.services.models.login.LoginResponse;
+import id.aicode.jalanaman.services.models.login.Place;
+
+/**
+ * Created by Ibam on 7/28/2017.
+ */
+
+public class MyPlaceAdapter extends RecyclerView.Adapter<MyPlaceAdapter.ViewHolder> {
+
+    Context context;
+    List<Place> placeList;
+
+    public MyPlaceAdapter(Context context, List<Place> list) {
+        this.context = context;
+        this.placeList = list;
+    }
+
+    public Context getContext() {
+        return this.context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_tab_my_place, parent, false);
+
+        return new MyPlaceAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Place place = placeList.get(position);
+
+        boolean isDanger = randomBool();
+
+        if (isDanger) {
+            holder.statusIcon.setImageDrawable(context.getDrawable(R.drawable.danger));
+        } else {
+            holder.statusIcon.setImageDrawable(context.getDrawable(R.drawable.safe));
+        }
+
+        String type;
+        if (place.getType().equals("L")) {
+            type = "";
+        } else {
+            type = "Rute ke ";
+        }
+
+        holder.placeTitle.setText(type + place.getName());
+
+        holder.placeFrom.setText(place.getPointALat() + ", " + place.getPointALong());
+        holder.placeTo.setText(place.getPointBLat() + ", " + place.getPointBLong());
+
+        if (!isDanger) {
+            holder.statusText.setBackground(context.getDrawable(R.color.color_safe));
+        }
+
+        if (!isDanger) {
+            holder.dangerLayout.setVisibility(View.GONE);
+        } else {
+            holder.dangerLocation.setText("Kebakaran di " + randomString());
+            holder.dangerTimestamp.setText("20 years ago");
+        }
+    }
+
+    private boolean randomBool() {
+        Random random = new Random();
+        return random.nextBoolean();
+    }
+
+    private String randomString() {
+        return UUID.randomUUID().toString().substring(0, 5);
+    }
+
+    public void setDataSet(List<Place> list) {
+        this.placeList = list;
+    }
+
+    public void addDataSet(List<Place> list) {
+        this.placeList.addAll(list);
+    }
+
+    public void addEvent(Place place) {
+        this.placeList.add(place);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.placeList.size();
+    }
+
+    private class ClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.icon_status_item_myplace)
+        ImageView statusIcon;
+
+        @BindView(R.id.title_item_myplace)
+        TextView placeTitle;
+
+        @BindView(R.id.placeName1_item_myplace)
+        TextView placeFrom;
+
+        @BindView(R.id.placeName2_item_myplace)
+        TextView placeTo;
+
+        @BindView(R.id.text_status_item_myplace)
+        TextView statusText;
+
+        @BindView(R.id.danger_information_layout_item_myplace)
+        LinearLayout dangerLayout;
+
+        @BindView(R.id.event_location_item_myplace)
+        TextView dangerLocation;
+
+        @BindView(R.id.event_time_item_myplace)
+        TextView dangerTimestamp;
+
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
--- app/src/main/java/id/aicode/jalanaman/myplace/MyPlaceTab.java
+++ app/src/main/java/id/aicode/jalanaman/myplace/MyPlaceTab.java
@@ -34,87 +34,12 @@ public class MyPlaceTab extends Fragment implements MyPlaceContract.View {
     @BindView(R.id.add_place)
     Button mButton;
 
-    @BindView(R.id.test_notif)
-    ImageView testNotif;
-
-
-    @BindBitmap(R.drawable.kebakaran)
-    Bitmap icon;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_my_place, container, false);
         ButterKnife.bind(this, view);
-
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
-                final View dialogView = getActivity().getLayoutInflater().inflate(R.layout
-                                .dialog_add_route,
-                        null);
-                View selectRoute = (View) dialogView.findViewById(R.id.item_select_route);
-                selectRoute.setVisibility(View.INVISIBLE);
-                RadioGroup radioGroup = (RadioGroup) dialogView.findViewById(R.id.radio_group);
-                radioGroup.setOnCheckedChangeListener (onCheckedChangeListener(dialogView));
-            dialogBuilder.setView(dialogView);
-            final AlertDialog dialog = dialogBuilder.create();
-            dialog.show();
-        }
+        return view;
     }
-
-    );
-
-    testNotif.setOnClickListener(new View.OnClickListener()
-
-    {
-        @Override
-        public void onClick (View v){
-
-// Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(getActivity(), MainActivity.class);
-
-// The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(getActivity());
-// Adds the back stack for the Intent (but not the Intent itself)
-        stackBuilder.addParentStack(MainActivity.class);
-// Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-
-        NotificationCompat.BigPictureStyle bigPictureStyle = new NotificationCompat
-                .BigPictureStyle();
-        bigPictureStyle.bigPicture(icon);
-        bigPictureStyle.setBigContentTitle("Kebakaran di RSUD Tangerang");
-        bigPictureStyle.bigLargeIcon(icon);
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(getActivity())
-                        .setSmallIcon(R.drawable.danger)
-                        .setLargeIcon(icon)
-                        .setContentTitle("Kebakaran di RSUD Tangerang")
-                        .setContentText("Swipe untuk melihat")
-                        .setColor(Color.WHITE)
-                        .setStyle(bigPictureStyle);
-        mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotificationManager =
-                (NotificationManager) getActivity().getSystemService(Context
-                        .NOTIFICATION_SERVICE);
-// mId allows you to update the notification later on.
-        mNotificationManager.notify(1, mBuilder.build());
-    }
-    }
-
-    );
-
-    return view;
-}
 
     private RadioGroup.OnCheckedChangeListener onCheckedChangeListener(final View dialogVIew) {
         RadioGroup.OnCheckedChangeListener listener = new RadioGroup.OnCheckedChangeListener() {
@@ -136,6 +61,29 @@ public class MyPlaceTab extends Fragment implements MyPlaceContract.View {
         };
 
         return listener;
+    }
+
+    public void getSavedItems(){
+
+    }
+
+    public void showSavedItems(){
+
+    }
+
+
+    public void showAddNewDialog(){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
+        final View dialogView = getActivity().getLayoutInflater().inflate(R.layout
+                        .dialog_add_route,
+                null);
+        View selectRoute = (View) dialogView.findViewById(R.id.item_select_route);
+        selectRoute.setVisibility(View.INVISIBLE);
+        RadioGroup radioGroup = (RadioGroup) dialogView.findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener(onCheckedChangeListener(dialogView));
+        dialogBuilder.setView(dialogView);
+        final AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
     }
 
 }
